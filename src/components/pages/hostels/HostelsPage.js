01d7ec7389f0580550b6d3042d04c2a73f4ced6f
adamdon <mail@adamdon.co.uk>
2021-11-29 07:30:17 +0000

    Hostels search refactor into it's own component

--- src/components/pages/hostels/HostelsPage.js
+++ src/components/pages/hostels/HostelsPage.js
@@ -4,13 +4,13 @@ import ContainerLayout from "../../containers/ContainerLayout";
 import ContainerContentRow from "../../containers/ContainerContentRow";
 import ContainerContent from "../../containers/ContainerContent";
 import HostelBox from "./HostelBox";
+import HostelsSearch from "./HostelsSearch";
 
 
 
 export default function HostelsPage()
 {
     const [data, setData] = useData();
-    const hostels2 = [{name: "test1", id: 1}, {name: "test2", id: 2}];
 
     useEffect( () =>
     {
@@ -43,24 +43,6 @@ export default function HostelsPage()
 
 
 
-    function handleFilterTextChange(e)
-    {
-        let filterText = e.target.value.toLowerCase();
-        setData({filterText: filterText});
-        console.log("handleFilterTextChange: " + filterText);
-        if(filterText === "")
-        {
-            setData({filteredHostels: data.hostels});
-        }
-        else
-        {
-            let newItems = data.hostels.filter(hostel => (hostel.name.toLowerCase() + " " + hostel.description.toLowerCase() + " " + hostel.postcode.toLowerCase()).includes(filterText));
-
-            setData({filteredHostels: newItems});
-        }
-    }
-
-
 
     return (
         <span>
@@ -69,20 +51,7 @@ export default function HostelsPage()
 
 
                 <ContainerContentRow>
-                    <ContainerContent size="12" icon="search" title="Search">
-
-                        <h4>Search for a hostel</h4>
-                        <p className="mb-3">
-                            Find a hostel for your next trip by looking up the name or description
-                        </p>
-                        <form action="" className="" onSubmit={e => e.preventDefault()}>
-                            <div className="input-group mb-3">
-                                <input type="text" className="form-control form-control-lg" placeholder="Search here" onChange={handleFilterTextChange}/>
-                                <button type="submit" className="input-group-text btn-success"><i className="fa fa-search me-2"></i> Search</button>
-                            </div>
-                        </form>
-
-                    </ContainerContent>
+                    <HostelsSearch/>
                 </ContainerContentRow>
 
 
