3372e39ed666fe5767803ae0f1355cd4f8cbe696
adamdon <mail@adamdon.co.uk>
2021-12-16 10:12:00 +0000

    ChangeData complete

--- src/components/pages/itineraries/ChangeDate.js
+++ src/components/pages/itineraries/ChangeDate.js
@@ -1,10 +1,13 @@
 import React, {useState} from "react";
+import {useData} from "../../data/DataContextProvider";
 
 
 
 
 export default function ChangeDate(props)
 {
+    const [data, setData] = useData();
+
     const [disabled, setDisabled] = useState(false);
     const [date, setDate] = useState(props.itinerary.startdate.substring(0, 10));
 
@@ -13,34 +16,52 @@ export default function ChangeDate(props)
     {
         if(event.key === "Enter")
         {
-            await submitButtonOnClick();
+            await changeButtonOnClick();
         }
     }
 
 
-    async function submitButtonOnClick()
+    async function changeButtonOnClick()
     {
+        setDisabled(true);
+        setData({showSpinner: true});
+
         console.log("submitButtonOnClick")
-        // if(user === "")
-        // {
-        //     setData({toastMessage: "Input 'user' before submitting"});
-        // }
-        // else if(!user.match(/^[0-9a-zA-Z\s]+$/))
-        // {
-        //     setData({toastMessage: "'user' can only contain letters, numbers and spaces"});
-        // }
-        // else if(data.hostels.length === 0)
-        // {
-        //     setData({toastMessage: "Still loading hostels"});
-        // }
-        // else if(data.itineraries.some(itinerary => itinerary.user.toLowerCase().trim() === user.toLowerCase().trim()))
-        // {
-        //     setData({toastMessage: "User name '" + user + "' already taken, enter new name"});
-        // }
-        // else
-        // {
-        //     await fetchAddItinerary();
-        // }
+        console.log(date);
+
+        if(date === "")
+        {
+            setData({toastMessage: "Set a date first, then click 'Change'"});
+        }
+        else
+        {
+            await fetchChangeDate(props.itinerary.user, new Date(date));
+        }
+
+
+        setDisabled(false);
+        setData({showSpinner: false});
+    }
+
+    async function fetchChangeDate(user, datetime)
+    {
+        let methodType = "GET"
+        let requestUrl = (data.config.baseUrl + "/itineraries/startdate/" + user + "/" + datetime.toISOString());
+        let requestHeaders = {"Content-Type": "application/json"};
+
+        const response = await fetch(requestUrl, {method: methodType, headers: requestHeaders});
+
+        if(Number(response.status.toString().substring(0, 1)) === 2) //check that response code starts with 2
+        {
+            const jsonData = await response.json();
+            console.log(requestUrl);
+            console.log(jsonData);
+            setData({toastSuccess: "Start date of " + user + "'s itinerary changed to " + datetime.toLocaleDateString()})
+        }
+        else
+        {
+            setData({toastError: "Error: " + response.status + " - Could not action"});
+        }
     }
 
 
@@ -54,7 +75,7 @@ export default function ChangeDate(props)
 
                 <input value={date} onChange={e => setDate(e.target.value)} onKeyDown={handleKeyDown} disabled={disabled} placeholder="Date" type="date" aria-label="review" id={"date"} className="form-control w-50"/>
 
-                <button onClick={submitButtonOnClick} disabled={disabled} className="btn btn-outline-light" type="button" id="change">Change</button>
+                <button onClick={changeButtonOnClick} disabled={disabled} className="btn btn-outline-light" type="button" id="change">Change</button>
             </div>
 
         </div>
