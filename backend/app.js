fb9932c7160cd9b6c825db6dea8babc381af7ac5
adamdon <mail@adamdon.co.uk>
2021-12-16 15:14:43 +0000

    deployment prep

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
-// var cors = require('cors');
+var cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(cors());
+app.use(cors());
 
 app.use('/', indexRouter);
 
